e893cdc1ba4496def5f1ac57ff0efd54f2542845
Hypnotiquee <47217508+Hypnotiquee@users.noreply.github.com>
2019-01-31 17:42:18 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,1 +1,16 @@
 ﻿#Mastermind Minigame Project By Ines x Vivien
+  color=[red,blue,green,yellow]
+longueur=input("Longueur de la ligne")
+tentatives=input("Nombre de tentatives")
+
+def Combinaison(color) :
+    comb=[]
+
+
+
+
+
+
+
+
+for i in range (0,(tentatives+1),1):
