62f0e44e22702481e31d43d3815c0b81b91f5209
Hypnotiquee <47217508+Hypnotiquee@users.noreply.github.com>
2019-02-07 16:19:29 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,11 +1,13 @@
+#initiation
+x=0
 color=[]
 lor=input("Nombre de couleurs")
 nbrcolor=int(lor)
 while nbrcolor!=0 :
-    x=1
+    x+=1
     color.append(x)
     nbrcolor=nbrcolor-1
-    x=x+1
+
 print(color)
 colonnes=input("Nombre de colonnes")
 tentatives=input("Nombre de tentatives")
