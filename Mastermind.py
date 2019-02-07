66192195bbcc5f4899b2335a5d4d2d32b5bdf612
Hypnotiquee <47217508+Hypnotiquee@users.noreply.github.com>
2019-02-07 16:16:13 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,15 +1,16 @@
-﻿#Mastermind Minigame Project By Ines x Vivien
 color=[]
-nbrcolor=input("Nombre de couleurs")
-while nbrcolor==0 :
+lor=input("Nombre de couleurs")
+nbrcolor=int(lor)
+while nbrcolor!=0 :
     x=1
     color.append(x)
     nbrcolor=nbrcolor-1
     x=x+1
-
-
+print(color)
 colonnes=input("Nombre de colonnes")
 tentatives=input("Nombre de tentatives")
+
+
 
 def Combinaison(color) :
     comb=[]
@@ -20,5 +21,6 @@ def Combinaison(color) :
 
 
 
-
+"""
 for i in range (0,(tentatives+1),1):
+"""
