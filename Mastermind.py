f771aaeedb47e55e75256f1dfb43b5246c75687f
Hypnotiquee <47217508+Hypnotiquee@users.noreply.github.com>
2019-04-11 17:09:19 +0200

    Update Mastermind.py
    do re mi fa sol la si do

--- Mastermind.py
+++ Mastermind.py
@@ -28,44 +28,53 @@ for i in range (0,NbrColonnes,1):
     OrdiCombinaison.append(y)
 print(OrdiCombinaison)
 
+
+
+def CombinaisonJoueur(JoueurCombinaison):
+    for i in range(NbrColonnes):
+        Tentative=input("Choisissez une couleur")
+        JoueurCombinaison.append(int(Tentative))
+    return JoueurCombinaison
+
+
+def PionsBienPlacés (OrdiCombinaison,JoueurCombinaison):
+    BienPlacés=0
+    for i in range(len(OrdiCombinaison)):
+        if JoueurCombinaison[i]==OrdiCombinaison[i]:
+            BienPlacés+=1
+    return BienPlacés
+
+def PionsMalPlacés (OrdiCombinaison,JoueurCombinaison):
+    OrdiCombinaisonCopie=[couleur for couleur in OrdiCombinaison]
+    Présentes=0
+    for i in range(len(JoueurCombinaison)):
+        Compare=JoueurCombinaison[i]
+        for i in range(len(OrdiCombinaisonCopie)):
+            if Compare==OrdiCombinaisonCopie[i]:
+                Présentes+=1
+                OrdiCombinaisonCopie[i]=0
+                break
+            else:
+                Présentes+=0
+    MalPlacés=Présentes-PionsBienPlacés(OrdiCombinaison,JoueurCombinaison)
+    return MalPlacés
+
 for i in range(0,NbrTentatives,1):
     #Combinaison du Joueur (Code Test)
     JoueurCombinaison=[]
-    def CombinaisonJoueur(JoueurCombinaison):
-        for i in range(NbrColonnes):
-            Tentative=input("Choisissez une couleur")
-            JoueurCombinaison.append(int(Tentative))
-        return JoueurCombinaison
     print(CombinaisonJoueur(JoueurCombinaison))
 
     #Boucle principale (Pas Fait)
-    if CombinaisonJoueur!=OrdiCombinaison:
+    if JoueurCombinaison!=OrdiCombinaison:
         #Pions Bien Placés (Code Final)
-        def PionsBienPlacés (OrdiCombinaison,JoueurCombinaison):
-            BienPlacés=0
-            for i in range(len(OrdiCombinaison)):
-                if JoueurCombinaison[i]==OrdiCombinaison[i]:
-                    BienPlacés+=1
-            return BienPlacés
+        
         print(PionsBienPlacés(OrdiCombinaison,JoueurCombinaison))
         #Pions Mal Placés (Code opérationnel)
         #Problème de Pions en plusieurs exemplaires comptés trop de fois réglé : Aide du prof
-        def PionsMalPlacés (OrdiCombinaison,JoueurCombinaison):
-            OrdiCombinaisonCopie=[couleur for couleur in OrdiCombinaison]
-            Présentes=0
-            for i in range(len(JoueurCombinaison)):
-                Compare=JoueurCombinaison[i]
-                for i in range(len(OrdiCombinaisonCopie)):
-                    if Compare==OrdiCombinaisonCopie[i]:
-                        Présentes+=1
-                        OrdiCombinaisonCopie[i]=0
-                        break
-                    else:
-                        Présentes+=0
-            MalPlacés=Présentes-PionsBienPlacés(OrdiCombinaison,JoueurCombinaison)
-            return MalPlacés
+        
         print(PionsMalPlacés(OrdiCombinaison,JoueurCombinaison))
 
     #Gagné (A Modifier ?)
-    elif NbrColonnes==PionsBienPlacés(OrdiCombinaison,JoueurCombinaison):
-        print("Vous avez gagné")
+    else :
+        print("marche")
+        break
