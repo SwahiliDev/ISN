388229d1bb835d39d20b6cc800a79a62d5b9eee0
Hypnotiquee <47217508+Hypnotiquee@users.noreply.github.com>
2019-05-02 17:52:01 +0200

    Update Masterchiffres.py
    Update: variables chiffres à la place de couleur

--- Masterchiffres.py
+++ Masterchiffres.py
@@ -1,20 +1,20 @@
 import random
 from random import randint
 
-#Nombre de couleurs
+#Nombre de chiffres
 x=0
-couleur_liste=[]
-couleur=input("Nombre de couleurs")
-nombre_couleur=int(couleur)
-while nombre_couleur!=0 :
+chiffres_liste=[]
+chiffres=input("Combien de chiffres différents (entre 1 et 9)")
+nombre_chiffres=int(chiffres)
+while nombre_chiffres!=0 :
     x+=1
-    couleur_liste.append(x)
-    nombre_couleur=nombre_couleur-1
+    chiffres_liste.append(x)
+    nombre_chiffres=nombre_chiffres-1
 
 #Autres
 colonnes=input("Nombre de colonnes")
 nombre_colonnes=int(colonnes)
-tentatives=input("Nombre de tentatives")
+tentatives=input("Nombre de chances")
 nombre_tentatives=int(tentatives)
 compteur=0
 gameover=1
@@ -22,30 +22,30 @@ gameover=1
 #Combinaison de l'Ordinateur
 ordi_combinaison=[]
 for i in range (0,nombre_colonnes,1):
-    y=randint(1,len(couleur_liste))
+    y=randint(1,len(chiffres_liste))
     ordi_combinaison.append(y)
-print ("Solution secrete", ordi_combinaison)
+print ("Combinaison à trouver", ordi_combinaison)
 
 #Combinaison du Joueur
 def CombinaisonJoueur():
     combinaison_joueur = []
-    selection_couleurs=input("Choisissez une couleur (ex 1224)")
-    if (len(selection_couleurs) == nombre_colonnes) :
+    selection_chiffres=input("Choisissez une combinaison (Exemple : 1234)")
+    if (len(selection_chiffres) == nombre_colonnes) :
         for i in range(nombre_colonnes):
-            combinaison_joueur.append(int(selection_couleurs[i]))
+            combinaison_joueur.append(int(selection_chiffres[i]))
     return combinaison_joueur #liste vide à tester si erreur d'entrée
 
-#Formules des pions bien placés
-def PionsBienPlacés(ordi_combinaison,joueur_combinaison):
+#Nombre de chiffres bien placés
+def ChiffresBienPlacés(ordi_combinaison,joueur_combinaison):
     bien_placés=0
     for i in range(len(ordi_combinaison)):
         if joueur_combinaison[i]==ordi_combinaison[i]:
             bien_placés+=1
     return bien_placés
 
-#Formules des pions mal placés (prof)
-def PionsMalPlacés(ordi_combinaison,joueur_combinaison):
-    ordi_combinaison_copie=[couleur for couleur in ordi_combinaison]
+#Nombre de chiffres mal placés (prof)
+def ChiffresMalPlacés(ordi_combinaison,joueur_combinaison):
+    ordi_combinaison_copie=[chiffres for chiffres in ordi_combinaison]
     présentes=0
     for i in range(len(joueur_combinaison)):
         compare=joueur_combinaison[i]
@@ -56,7 +56,7 @@ def PionsMalPlacés(ordi_combinaison,joueur_combinaison):
                 break
             else:
                 présentes+=0
-    malplacés=présentes-PionsBienPlacés(ordi_combinaison,joueur_combinaison)
+    malplacés=présentes-ChiffresBienPlacés(ordi_combinaison,joueur_combinaison)
     return malplacés
 
 #Boucle principale
@@ -64,15 +64,15 @@ while nombre_tentatives!=0 and gameover != 0:
     #Affichage de l'historique des combinaisons
     joueur_combinaison=CombinaisonJoueur()
     if len(joueur_combinaison) == 0 :
-        print("Attention à introduire le bon nombre de couleurs !")
+        print("Attention, il faut que vous entriez:",nombre_colonnes,"chiffres !")
     else :
-        print("Votre proposition n° ", compteur+1, " est : ", joueur_combinaison)
+        print("Votre proposition n°", compteur+1, "est : ", joueur_combinaison)
         nombre_tentatives-=1
         compteur+=1
         #Boucle de comparaison
         if joueur_combinaison!=ordi_combinaison:
-            print(PionsBienPlacés(ordi_combinaison,joueur_combinaison))
-            print(PionsMalPlacés(ordi_combinaison,joueur_combinaison))
+            print("Vous avez:",ChiffresBienPlacés(ordi_combinaison,joueur_combinaison),"chiffres de bien placés")
+            print("Vous avez:",ChiffresMalPlacés(ordi_combinaison,joueur_combinaison),"chiffres de mal placés")
         #Message de victoire
         else :
             print("Vous avez gagné en ",compteur,"tentative(s)")
@@ -80,3 +80,4 @@ while nombre_tentatives!=0 and gameover != 0:
 #Message de défaite
 if gameover==1:
     print("Perdu, vous avez utilisé vos",compteur,"tentative(s)")
+
